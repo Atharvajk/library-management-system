8bbe0fa474a9ee7deab042930ad3144b498c7deb
Atharvajk <atharva.j.khodke@gmail.com>
2024-09-27 01:26:10 +0530

    LibraryController: Added routes in app

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-
+const {
+    addBook,
+    borrowBook,
+    returnBook,
+    viewAvailableBooks
+  } = require('./src/Controller/LibraryController');
+  
 app.use(bodyParser.json());
+app.post('/books', addBook);
+app.put('/books/borrow/:isbn', borrowBook);
+app.put('/books/return/:isbn', returnBook);
+app.get('/books', viewAvailableBooks);
 
 // Server setup
 app.listen((3000), () => {
